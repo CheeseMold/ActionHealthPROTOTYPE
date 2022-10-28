5be8cdb9975cc2c13055a67bdfca719e68eec592
CheeseMold <henriquebear17@gmail.com>
2022-10-28 16:19:40 -0300

    Initial testing of features

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+            </View>
+
+            <StatusBar style="auto" />
+            <View style={styles.img}>
+                <TouchableOpacity onPress={() => console.log("Image clicked.")}>
+                    <Image
+                        style={{ borderRadius: 20 }}
+                        source={{
+                            uri: "https://picsum.photos/200/300",
+                            width: 200,
+                            height: 300,
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "dodgerblue",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    img: {
+        flex: 2,
+        allignItems: "center",
+        backgroundColor: "dodgerblue",
+    },
 });
